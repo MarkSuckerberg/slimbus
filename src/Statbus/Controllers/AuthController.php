313b9694461381de1be9db2ab3abfa8ef0929ab7
Mark Suckerberg <mark@suckerberg.gay>
2024-11-18 21:02:39 -0600

    Swaps the old auth controller back in

--- src/Statbus/Controllers/AuthController.php
+++ src/Statbus/Controllers/AuthController.php
@@ -1,71 +1,137 @@
 <?php
 
 namespace Statbus\Controllers;
-use Jumbojett\OpenIDConnectClient;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseInterface;
 use Statbus\Controllers\Controller as Controller;
+use GuzzleHttp\Client;
 
 
 class AuthController extends Controller
 {
 
-  private OpenIDConnectClient $oidc;
+  // protected $DB;
+
+  private $site_private_token;
+  private $session_private_token;
+  private $session_public_token;
+  private $return_uri;
+  private $AuthUrl;
+  private $TokenUrl;
+  private $AuthSessionUrl;
+
+  private string $remote;
 
 
   public function __construct(ContainerInterface $container)
   {
     parent::__construct($container);
-
     $this->settings = $container->get('settings')['statbus']['auth'];
-
-    if (!isset($settings['remote_auth'])) {
+    if (!$this->settings['remote_auth']) {
       return;
-    }
+    } else {
+      $this->remote = $this->settings['remote_auth'];
+      $this->AuthUrl = $this->remote . "oauth_create_session.php";
+      $this->TokenUrl = $this->remote . "oauth.php";
+      $this->AuthSessionUrl = $this->remote . "oauth_get_session_info.php";
+      $this->return_uri = $this->router->pathFor('auth_return');
 
-    $this->oidc = new OpenIDConnectClient(
-      $this->settings['remote_auth'],
-      $this->settings['client_id'],
-      $this->settings['client_secret']
-    );
-    $this->oidc->setCertPath($this->settings['cert_path']);
+      if (isset($_SESSION['site_private_token'])) {
+        $this->site_private_token = $_SESSION['site_private_token'];
+      } else {
+        $this->site_private_token = $this->generateToken(TRUE);
+        $_SESSION['site_private_token'] = $this->site_private_token;
+      }
+    }
+  }
+
+  public function generateToken(bool $secure = TRUE)
+  {
+    $r_bytes = openssl_random_pseudo_bytes(5120, $secure);
+    return hash('sha512', $r_bytes);
   }
 
   public function auth($request, $response, $args)
   {
     if (!$this->settings['remote_auth']) {
-      return $this->view->render($this->response, 'base/error.tpl', [
+      return $this->view->render($response, 'base/error.tpl', [
         'message' => "Authentication not supported",
         'code' => 501,
       ]);
     }
+    $client = new Client();
+    $res = $client->request('GET', $this->AuthUrl, [
+      'query' => [
+        'site_private_token' => $this->site_private_token,
+        'return_uri' => $this->return_uri,
+      ]
+    ]);
+    $res = json_decode($res->getBody());
+
+    $this->response = $res;
+    $_SESSION['session_public_token'] = $res->session_public_token;
+    $_SESSION['session_private_token'] = $res->session_private_token;
+    $this->session_public_token = $_SESSION['session_public_token'];
+    $this->session_private_token = $_SESSION['session_private_token'];
 
     return $this->view->render($response, 'auth/confirm.tpl');
   }
 
-  public function auth_redirect($request, ResponseInterface $response, $args)
+  public function auth_redirect($request, $response, $args)
   {
-    if (!$this->oidc->authenticate()) {
-      //Authenticate() sets the location header to the redirect URI if false
-      return $response->withStatus(302);
-    }
+    $this->session_public_token = $_SESSION['session_public_token'];
+    $this->session_private_token = $_SESSION['session_private_token'];
 
-    $ckey = $this->oidc->getVerifiedClaims('ckey');
-    if (!isset($ckey)) {
-      return $this->view->render($response, 'base/error.tpl', [
-        'message' => 'Ckey verification failed.',
-        'code' => 403
-      ]);
-    }
+    return $response->withRedirect("$this->TokenUrl?session_public_token=" . urlencode($this->session_public_token));
+  }
 
-    //Get a new ID when logging in
-    regenerate_statbus_session();
-    $_SESSION['ckey'] = $ckey;
-
-
-    return $this->view->render($response, 'auth/return.tpl', [
-      'return_uri' => $_SESSION['return_uri'] ?: false
+  public function auth_return($request, $response, $args)
+  {
+    $this->site_private_token = $_SESSION['site_private_token'];
+    $this->session_private_token = $_SESSION['session_private_token'];
+    $client = new Client();
+    $res = $client->request('GET', $this->AuthSessionUrl, [
+      'query' => [
+        'site_private_token' => $this->site_private_token,
+        'session_private_token' => $this->session_private_token,
+      ]
     ]);
+    $res = json_decode($res->getBody());
+    if ('OK' != $res->status) {
+      die("Something went wrong!");
+    }
+    foreach ($res as $k => $v) {
+      $_SESSION['sb'][$k] = $v;
+    }
+    if (isset($_SESSION['return_uri'])) {
+      $return_uri = $_SESSION['return_uri'];
+    } else {
+      $return_uri = false;
+    }
+    return $this->view->render($response, 'auth/return.tpl', [
+      'return_uri' => $return_uri
+    ]);
+  }
+
+  public function doubleCheckRemote()
+  {
+    if ($_SESSION['canary'] < time() - 300) {
+      $client = new Client();
+      $res = $client->request('GET', $this->AuthSessionUrl, [
+        'query' => [
+          'site_private_token' => $this->site_private_token,
+          'session_private_token' => $this->session_private_token,
+        ]
+      ]);
+      $res = json_decode($res->getBody());
+      if ('OK' != $res->status) {
+        session_unset();
+        session_destroy();
+        session_start();
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 
   public function logout($request, $response, $args)
